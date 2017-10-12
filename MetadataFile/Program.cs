933a216c28f56352f0810bee1b8f94a001b75b84
David <david@ubilogix.com>
2017-10-12 12:13:51 -0700

    first commit

---
+++ MetadataFile/Program.cs
@@ -0,0 +1,44 @@
+﻿using DSOFile;
+using System;
+using System.Collections.Generic;
+
+namespace MetadataFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = args.Length > 0 ? args[0] : @"C:\Users\David\Configuration.db";
+
+            OleDocumentPropertiesClass file = new OleDocumentPropertiesClass();
+
+            Console.WriteLine("Opening file: {0}", filePath);
+            file.Open(filePath, false, dsoFileOpenOptions.dsoOptionDefault);
+
+            SetCustomProperty(file.CustomProperties, "Packets", 65);
+            SetCustomProperty(file.CustomProperties, "Powered by", "Rey David Dominguez");
+
+            foreach (CustomProperty p in file.CustomProperties)
+            {
+                Console.WriteLine("{0}:{1}", p.Name, p.get_Value().ToString());
+            }
+
+            file.Close(true);
+            Console.ReadKey();
+        }
+        
+        public static void SetCustomProperty(CustomProperties customProperties, string property, object value)
+        {
+            foreach(CustomProperty customProperty in customProperties)
+            {
+                if(customProperty.Name == property)
+                {
+                    customProperty.set_Value(ref value);
+                    return;
+                }
+            }
+
+            customProperties.Add(property, ref value);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Metadata of File
